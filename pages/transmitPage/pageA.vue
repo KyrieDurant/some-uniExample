4f70b0421f035051028e1474d9498c0bb77318f4
KyrieDurant <1968415571@qq.com>
2022-12-20 17:27:22 +0800

    A页面新增画图案例

--- pages/transmitPage/pageA.vue
+++ pages/transmitPage/pageA.vue
@@ -4,10 +4,20 @@
 		<view class="jumpBpage" @click="getBpage(id)">跳转B页面</view>
 		<view class="playMusic" @click="openPop()">自定义弹窗</view>
 		<view class="changecolorBox">
-			<view class=""></view>
-			<view class=""></view>
-			<view class=""></view>
-			<view class=""></view>
+			<!-- css动画绕着方框走 -->
+			<view class="around"></view>
+			<view class="xuanzBox">
+				<view class="changfx"></view>
+			</view>
+			
+		</view>
+		<view class="changecolorBox">
+			<!-- 画三角形 -->
+			<view class="sjxBox">
+				<view class="triangle"></view>
+			</view>
+			<!-- 圆形 -->
+			<view class="circle"></view>
 		</view>
 	</view>
 </template>
@@ -17,19 +27,19 @@
 		data() {
 			return {
 				id: '11',
-				
+
 			}
 		},
 		onLoad() {
-			this.getmergeArray();//数组合并小案例
+			this.getmergeArray(); //数组合并小案例
 		},
 		methods: {
-			getBpage(id){
+			getBpage(id) {
 				uni.navigateTo({
-					url:'/pages/transmitPage/pageB?id='+id,
+					url: '/pages/transmitPage/pageB?id=' + id,
 				})
 			},
-			getmergeArray(){
+			getmergeArray() {
 				// 进行数组合并的方法:
 				//1、运用for循环把arr2里的元素合并成
 				// let arr1 = [1,2];
@@ -39,20 +49,20 @@
 				// }
 				// console.log('arr1',arr1);
 				//log打印得到arr1[1,2,3,4];
-				
+
 				//2、concat方法更新数组
 				// arr1 = arr1.concat(arr2);
-				
+
 				//3、push.apply方法更新数组
 				// arr1.push.apply(arr1, arr2);
-				
+
 				//4、ES6写法合并数组
 				// arr1 = [...arr1,...arr2];
-				
+
 				//5、push结合ES6写法合并数组
 				// arr1.push(...arr2);
-				
-				
+
+
 				//删除数组内元素的方法
 				// 1.这个函数扩展了JavaScript的内置对象Array，这样，我们以后的所有声明的数组都会自动的拥有remove能力
 				// Array.prototype.removeByValue = function (val) {
@@ -68,7 +78,7 @@
 				// sites.removeByValue('google');
 				// console.log(sites);
 				// -> ['apple', 'runoob', 'taobao']
-				
+
 				//2、array.splice 的方法实现
 				// const array = [2, 5, 9, 11];
 				// const index = array.indexOf(2);
@@ -77,29 +87,29 @@
 				// }
 				// console.log('array',array);
 				// // array = [2, 9]
-				
+
 				//3、delete关键字,删除数组的值但是它的位置还存在，为empty,arr的长度仍然为4
 				// var arr = [1, 2, 3, 4];
 				// delete arr[0];
 				// console.log(arr);   //[empty, 2, 3, 4]
-				
+
 				//4、栈方法
-				var colors = ["A", "B", "C","D"];
+				var colors = ["A", "B", "C", "D"];
 				var item = colors.pop();
-				console.log(item);      //"C"
-				console.log(colors.length);    //2
-				
+				console.log(item); //"C"
+				console.log(colors.length); //2
+
 				//5、splice()方法删除
 				// var colors = ["red", "blue", "grey"];
 				// var item = colors.splice(0, 1);
 				// console.log(item);      //"red"
 				// console.log(colors);    //["blue", "grey"]
 			},
-			
-			openPop(){
+
+			openPop() {
 				console.log('自定义弹窗');
 				uni.navigateTo({
-					url:'/pages/transmitPage/musicPop',
+					url: '/pages/transmitPage/musicPop',
 				})
 			},
 		}
@@ -107,49 +117,5 @@
 </script>
 
 <style>
-	.content{
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-	}
-	.dataId{
-		width: 100%;
-		height: 100rpx;
-		background-color: #e8eef3;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
-	.jumpBpage{
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		width: 300rpx;
-		height: 77rpx;
-		margin-top: 60rpx;
-		color: #fff;
-		background-color: rgb(0, 64, 255);
-		border: 5rpx solid rgb(102, 102, 255);
-		border-radius: 15rpx;
-	}
-	.playMusic{
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		width: 300rpx;
-		height: 77rpx;
-		margin-top: 60rpx;
-		color: #fff;
-		background-color: rgb(0, 64, 255);
-		border: 5rpx solid rgb(102, 102, 255);
-		border-radius: 15rpx;
-	}
-	.changecolorBox{
-		width: 80%;
-		height: 300rpx;
-		margin-top: 50rpx;
-		border: 1rpx solid #000000;
-	}
-	
+	@import url("pageA.css");
 </style>
